ec97c67da01a2d9aa7ce0728e46025384058a425
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-26 15:04:15 +0530

    added

--- src/EmployeePayrollService.java
+++ src/EmployeePayrollService.java
@@ -1,80 +1,59 @@
-
-
-	import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchEvent.Kind;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Java8WatchServiceExample {
-	private static final Kind<?> ENTRY_CREATE = null;
-	private static final Kind<?> ENTRY_DELETE = null;
-	private static final Kind<?> ENTRY_MODIFY = null;
-	private final WatchService watcher;
-	private final Map<WatchKey, Path> dirWatchers;
-
-	Java8WatchServiceExample(Path dir) throws IOException {
-		this.watcher = FileSystems.getDefault().newWatchService();
-		this.dirWatchers = new HashMap<WatchKey, Path>();
-		scanAndRegisterDirectories(dir);
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class EmployeePayRollService {
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
 	}
 
-	private void scanAndRegisterDirectories(final Path start) throws IOException {
-		Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-				registerDirWatchers(dir);
-				return FileVisitResult.CONTINUE;
-			}
-		});
+	private List<EmployeePayRollData> employeePayrollList;
+	//public EmployeePayRollService() {};
+
+	public EmployeePayRollService() {
+	};
+
+	public EmployeePayRollService(List<EmployeePayRollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+	public static void main(String[] args) {
+		ArrayList<EmployeePayRollData> employeePayrollList = new ArrayList<>();
+		EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayrollList);
+		Scanner sc = new Scanner(System.in);
+		employeePayRollService.readData(sc);
+		employeePayRollService.writeData();
+		employeePayRollService.writeData(null);
 	}
 
-	private void registerDirWatchers(Path dir) throws IOException {
-		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-		dirWatchers.put(key, dir);
+	private void readData(Scanner sc) {
+	public void readData(Scanner sc) {
+		System.out.println("Enter ID:");
+		int id = sc.nextInt();
+		System.out.println("Enter Name:");
+		String name = sc.next();
+		System.out.println("Enter Salary:");
+		double salary = sc.nextDouble();
+		employeePayrollList.add(new EmployeePayRollData(id, name, salary));
 	}
-	@SuppressWarnings({"rawtypes","unchecked"})
-	void processEvents() {
-		while (true) {
-			WatchKey key;
-			try {
-				key = watcher.take();
-			} catch (InterruptedException x) {
-				return;
-			}
-			Path dir = dirWatchers.get(key);
-			if (dir == null)
-				continue;
-			for (WatchEvent<?> event : key.pollEvents()) {
-				WatchEvent.Kind kind = event.kind();
-				Path name = ((WatchEvent<Path>) event).context();
-				Path child = dir.resolve(name);
-				System.out.format("%s: %s\n", event.kind().name(), child);
-				if (kind == ENTRY_CREATE) {
-					try {
-						if (Files.isDirectory(child))
-							scanAndRegisterDirectories(child);
-					} catch (IOException x) {}
-				} else if (kind.equals(ENTRY_DELETE)) {
-					if (Files.isDirectory(child))
-						dirWatchers.remove(key);
-				}
-			}
-			boolean valid = key.reset();
-			if (!valid) {
-				dirWatchers.remove(key);
-				if (dirWatchers.isEmpty())
-					break;
-			}
-		}
+
+	private void writeData() {
+		System.out.println(employeePayrollList);
+	}
+	public void writeData(IOService ioService) {
+		if (ioService.equals(com.bridgelabz.employee.EmployeePayRollService.IOService.CONSOLE_IO))
+			System.out.println(employeePayrollList);
+		else if (ioService.equals(com.bridgelabz.employee.EmployeePayRollService.IOService.FILE_IO))
+			new EmployeePayrollFileIOService().writeData2(employeePayrollList);
+	}
+
+	public void printData(IOService ioService) {
+		if(ioService.equals(com.bridgelabz.employee.EmployeePayRollService.IOService.FILE_IO))
+			new com.bridgelabz.employee.EmployeePayrollFileIOService().printData();
+	}
+
+	public long countEntries(IOService ioService) {
+		if(ioService.equals(com.bridgelabz.employee.EmployeePayRollService.IOService.FILE_IO))
+			return new com.bridgelabz.employee.EmployeePayrollFileIOService().countEntries();
+		return 0;
+
 	}
 }
